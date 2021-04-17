606a4f81b7545b911c8777b2c3e787c23e00d052
NeotenicPrimate <vladimir.g.borel@gmail.com>
2021-04-17 19:37:58 +0200

    .env

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+print(os.environ['USERNAME'])
+print(os.environ['PASSWORD'])
+
+
